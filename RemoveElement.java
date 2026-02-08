3cb7b59491b98e810c83d9ec4ed21172a588f3dc
leodlcg <142692721+leodlcg@users.noreply.github.com>
2026-02-08 19:11:48 -0300

    Create RemoveElement.java

---
+++ RemoveElement.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/remove-element/submissions/1912886822/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
